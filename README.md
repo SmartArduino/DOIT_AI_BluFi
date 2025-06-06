1fbf7d3a40ec57a521786a2b565e60f8c530f18c
hanruo <552455797@qq.com>
2025-06-06 14:40:05 +0800

    first

---
+++ README.md
@@ -0,0 +1,34 @@
+# DOIT_AI_BluFi
+
+*   首先致谢虾哥的开源项目：https://github.com/78/xiaozhi-esp32
+*   其次致谢：https://github.com/xinnan-tech/xiaozhi-esp32-server
+
+
+## 项目简介
+ESP32-C3作为主控，驱动240*240屏幕，使用blufi一键配网绑定，软件硬件全开源
+
+特色：
+
+1. 实现通过蓝牙BluFi的方式给小智配网。
+2. 小程序自动添加设备。
+3. 完全兼容 xiaozhi-esp32-server.
+
+
+## 效果视频
+
+## 使用说明
+1. 获取代码：git clone https://github.com/SmartArduino/DOIT_AI_BluFi.git
+2. 使用vscode打开工程（需espidf版本>5.3.2）,设置目标芯片为esp32s3，命令：idf.py set-target esp32c3
+3. 编译工程：idf.py build
+4. 修改menuconfig:idf.py menuconfig
+    将Xiaozhi Assistant->Board Type设置为doit-ai-02-kit-lcd
+5. 烧录代码:idf.py flash
+6. 使用小程序小智小助理blufi配网
+![alt text](image1.jpg)
+
+
+## 技术支持
+![alt text](image.png)
+
+## 购买链接
+购买地址：https://item.taobao.com/item.htm?id=904320615994
---
+++ main/boards/common/wifi_board.cc
@@ -0,0 +1,293 @@
+#include "wifi_board.h"
+
+#include "display.h"
+#include "application.h"
+#include "system_info.h"
+#include "doit_blufi.h"
+
+#include "font_awesome_symbols.h"
+#include "settings.h"
+#include "assets/lang_config.h"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_http.h>
+#include <esp_mqtt.h>
+#include <esp_udp.h>
+#include <tcp_transport.h>
+#include <tls_transport.h>
+#include <web_socket.h>
+#include <esp_log.h>
+
+#include <wifi_station.h>
+#include <wifi_configuration_ap.h>
+#include <ssid_manager.h>
+
+static const char *TAG = "WifiBoard";
+
+WifiBoard::WifiBoard() {
+    Settings settings("wifi", true);
+    wifi_config_mode_ = settings.GetInt("force_ap") == 1;
+    if (wifi_config_mode_) {
+        ESP_LOGI(TAG, "force_ap is set to 1, reset to 0");
+        settings.SetInt("force_ap", 0);
+    }
+}
+
+std::string WifiBoard::GetBoardType() {
+    return "wifi";
+}
+
+void WifiBoard::EnterWifiConfigMode() {
+    auto& application = Application::GetInstance();
+    application.SetDeviceState(kDeviceStateWifiConfiguring);
+
+    // auto& wifi_ap = WifiConfigurationAp::GetInstance();
+    // wifi_ap.SetLanguage(Lang::CODE);
+    // wifi_ap.SetSsidPrefix("Xiaozhi");
+    // wifi_ap.Start();
+    doit_blufi_init();
+    // 显示 WiFi 配置 AP 的 SSID 和 Web 服务器 URL
+    std::string hint = "使用小智小助理添加设备";
+
+    hint += "\n\n";
+    
+    // 播报配置 WiFi 的提示
+    application.Alert(Lang::Strings::WIFI_CONFIG_MODE, hint.c_str(), "", Lang::Sounds::P3_WIFICONFIG);
+    
+
+}
+Ota ota_;
+
+void WifiBoard::StartNetwork() {
+    // User can press BOOT button while starting to enter WiFi configuration mode
+    uint8_t is_config = 0;
+    bool has_config = blufi_storage_read_has_config();
+
+    auto display = Board::GetInstance().GetDisplay();
+    if( has_config == 0) {
+        // If not configured, enter WiFi configuration mode
+        EnterWifiConfigMode();
+    } else {
+        // Otherwise, start the WiFi station
+        is_config = 1;
+
+        // auto& wifi_station = WifiStation::GetInstance();
+        // wifi_station.Start();
+        blufi_wifi_start_connect() ;
+        std::string notification = Lang::Strings::CONNECT_TO;
+        notification += "wifi";
+        notification += "...";
+        display->ShowNotification(notification.c_str(), 30000);
+    }
+
+    const int MAX_RETRY = 3;
+    int retry_count = 0;
+    int retry_delay = 10; // 初始重试延迟为10秒
+    uint8_t wait_cnt = 0;
+    while (1) {
+        wait_cnt += 1;
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        if (blufi_wifi_sta_get_connect_status()) {
+            if(is_config == 0){
+                vTaskDelay(pdMS_TO_TICKS(100));
+                if (!ota_.CheckVersion()) {
+                    retry_count++;
+                    if (retry_count >= MAX_RETRY) {
+                        ESP_LOGE(TAG, "Too many retries, exit version check");
+                        return;
+                    }
+        
+                    ESP_LOGW(TAG, "Check new version failed, retry in %d seconds (%d/%d)", retry_delay, retry_count, MAX_RETRY);
+                    for (int i = 0; i < retry_delay; i++) {
+                        vTaskDelay(pdMS_TO_TICKS(1000));
+                      
+                    }
+                    retry_delay *= 2; // 每次重试后延迟时间翻倍
+                    continue;
+                }
+                auto& message = ota_.GetActivationMessage();
+                auto& code = ota_.GetActivationCode();
+                ESP_LOGI(TAG, "Activation code: %s", code.c_str());
+                doit_blufi_send_code((uint8_t *)code.c_str());
+                esp_restart();
+                // esp_restart();
+            }
+            break;
+        }
+        
+        if(wait_cnt > 60){
+            EnterWifiConfigMode();
+            return;
+        }
+    }
+
+    std::string conn_notification = Lang::Strings::CONNECTED_TO;
+    display->ShowNotification(conn_notification.c_str(), 30000);
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+}
+
+Http* WifiBoard::CreateHttp() {
+    return new EspHttp();
+}
+
+WebSocket* WifiBoard::CreateWebSocket() {
+    Settings settings("websocket", false);
+    std::string url = settings.GetString("url");
+    if (url.find("wss://") == 0) {
+        return new WebSocket(new TlsTransport());
+    } else {
+        return new WebSocket(new TcpTransport());
+    }
+    return nullptr;
+}
+
+Mqtt* WifiBoard::CreateMqtt() {
+    return new EspMqtt();
+}
+
+Udp* WifiBoard::CreateUdp() {
+    return new EspUdp();
+}
+
+const char* WifiBoard::GetNetworkStateIcon() {
+    if (wifi_config_mode_) {
+        return FONT_AWESOME_WIFI;
+    }
+    auto& wifi_station = WifiStation::GetInstance();
+    if (!wifi_station.IsConnected()) {
+        return FONT_AWESOME_WIFI_OFF;
+    }
+    int8_t rssi = wifi_station.GetRssi();
+    if (rssi >= -60) {
+        return FONT_AWESOME_WIFI;
+    } else if (rssi >= -70) {
+        return FONT_AWESOME_WIFI_FAIR;
+    } else {
+        return FONT_AWESOME_WIFI_WEAK;
+    }
+}
+
+std::string WifiBoard::GetBoardJson() {
+    // Set the board type for OTA
+    auto& wifi_station = WifiStation::GetInstance();
+    std::string board_json = std::string("{\"type\":\"" BOARD_TYPE "\",");
+    board_json += "\"name\":\"" BOARD_NAME "\",";
+    if (!wifi_config_mode_) {
+        board_json += "\"ssid\":\"" + wifi_station.GetSsid() + "\",";
+        board_json += "\"rssi\":" + std::to_string(wifi_station.GetRssi()) + ",";
+        board_json += "\"channel\":" + std::to_string(wifi_station.GetChannel()) + ",";
+        board_json += "\"ip\":\"" + wifi_station.GetIpAddress() + "\",";
+    }
+    board_json += "\"mac\":\"" + SystemInfo::GetMacAddress() + "\"}";
+    return board_json;
+}
+
+void WifiBoard::SetPowerSaveMode(bool enabled) {
+    auto& wifi_station = WifiStation::GetInstance();
+    wifi_station.SetPowerSaveMode(enabled);
+}
+
+void WifiBoard::ResetWifiConfiguration() {
+    // Set a flag and reboot the device to enter the network configuration mode
+    {
+        Settings settings("wifi", true);
+        settings.SetInt("force_ap", 1);
+    }
+    GetDisplay()->ShowNotification(Lang::Strings::ENTERING_WIFI_CONFIG_MODE);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    // Reboot the device
+    esp_restart();
+}
+
+std::string WifiBoard::GetDeviceStatusJson() {
+    /*
+     * 返回设备状态JSON
+     * 
+     * 返回的JSON结构如下：
+     * {
+     *     "audio_speaker": {
+     *         "volume": 70
+     *     },
+     *     "screen": {
+     *         "brightness": 100,
+     *         "theme": "light"
+     *     },
+     *     "battery": {
+     *         "level": 50,
+     *         "charging": true
+     *     },
+     *     "network": {
+     *         "type": "wifi",
+     *         "ssid": "Xiaozhi",
+     *         "rssi": -60
+     *     },
+     *     "chip": {
+     *         "temperature": 25
+     *     }
+     * }
+     */
+    auto& board = Board::GetInstance();
+    auto root = cJSON_CreateObject();
+
+    // Audio speaker
+    auto audio_speaker = cJSON_CreateObject();
+    auto audio_codec = board.GetAudioCodec();
+    if (audio_codec) {
+        cJSON_AddNumberToObject(audio_speaker, "volume", audio_codec->output_volume());
+    }
+    cJSON_AddItemToObject(root, "audio_speaker", audio_speaker);
+
+    // Screen brightness
+    auto backlight = board.GetBacklight();
+    auto screen = cJSON_CreateObject();
+    if (backlight) {
+        cJSON_AddNumberToObject(screen, "brightness", backlight->brightness());
+    }
+    auto display = board.GetDisplay();
+    if (display && display->height() > 64) { // For LCD display only
+        cJSON_AddStringToObject(screen, "theme", display->GetTheme().c_str());
+    }
+    cJSON_AddItemToObject(root, "screen", screen);
+
+    // Battery
+    int battery_level = 0;
+    bool charging = false;
+    bool discharging = false;
+    if (board.GetBatteryLevel(battery_level, charging, discharging)) {
+        cJSON* battery = cJSON_CreateObject();
+        cJSON_AddNumberToObject(battery, "level", battery_level);
+        cJSON_AddBoolToObject(battery, "charging", charging);
+        cJSON_AddItemToObject(root, "battery", battery);
+    }
+
+    // Network
+    auto network = cJSON_CreateObject();
+    auto& wifi_station = WifiStation::GetInstance();
+    cJSON_AddStringToObject(network, "type", "wifi");
+    cJSON_AddStringToObject(network, "ssid", wifi_station.GetSsid().c_str());
+    int rssi = wifi_station.GetRssi();
+    if (rssi >= -60) {
+        cJSON_AddStringToObject(network, "signal", "strong");
+    } else if (rssi >= -70) {
+        cJSON_AddStringToObject(network, "signal", "medium");
+    } else {
+        cJSON_AddStringToObject(network, "signal", "weak");
+    }
+    cJSON_AddItemToObject(root, "network", network);
+
+    // Chip
+    float esp32temp = 0.0f;
+    if (board.GetTemperature(esp32temp)) {
+        auto chip = cJSON_CreateObject();
+        cJSON_AddNumberToObject(chip, "temperature", esp32temp);
+        cJSON_AddItemToObject(root, "chip", chip);
+    }
+
+    auto json_str = cJSON_PrintUnformatted(root);
+    std::string json(json_str);
+    cJSON_free(json_str);
+    cJSON_Delete(root);
+    return json;
+}
